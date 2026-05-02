e56dfa25d6f681b8213fde5276e11b4764a4b5a7
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-02 10:13:22 +0700

    chore: skip i18n pot file generation on undistributable packages
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/make-pot.sh
+++ scripts/make-pot.sh
@@ -9,5 +9,10 @@ for pkg_dir in packages/*/; do
     pkg_dir="${pkg_dir%/}"
     pkg="${pkg_dir##*/}"
 
+    if [ ! -f "$pkg_dir/.distignore" ]; then
+        echo -e "\e[1;33mNotice:\e[0m No .distignore found for $pkg, skipping"
+        continue
+    fi
+
     _wp i18n make-pot "$pkg_dir" "$pkg_dir/languages/$pkg.pot"
 done
