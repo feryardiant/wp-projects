a8f6724cc69a807f8717f2eaebea7d6666338dd6
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-05-17 20:08:56 +0700

    feat(ci): automate release workflows
    Related PRs:
    - projek-xyz/actions#24
    - projek-xyz/actions#25
    - projek-xyz/actions#26
    - feryardiant/wp-env#21
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/local-env.php
+++ scripts/local-env.php
@@ -2,13 +2,15 @@
 
 $root_path = dirname(__DIR__);
 
-require_once $root_path . '/vendor/autoload.php';
+if ( file_exists($root_path . '/vendor/autoload.php') ) {
+    require_once $root_path . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable($root_path);
+    if ( file_exists($root_path . '/.env') ) {
+        Dotenv\Dotenv::createImmutable($root_path)->safeLoad();
+    }
+}
 
-$dotenv->safeLoad();
-
-putenv("WORDPRESS_DB_NAME={$_ENV['DB_NAME']}");
-putenv("WORDPRESS_DB_USER={$_ENV['DB_USER']}");
-putenv("WORDPRESS_DB_PASSWORD={$_ENV['DB_PASS']}");
-putenv("WORDPRESS_DB_HOST=127.0.0.1:{$_ENV['FORWARD_DB_PORT']}");
+putenv("WORDPRESS_DB_NAME=" . ($_ENV['DB_NAME'] ?? 'wordpress'));
+putenv("WORDPRESS_DB_USER=" . ($_ENV['DB_USER'] ?? 'secret'));
+putenv("WORDPRESS_DB_PASSWORD=" . ($_ENV['DB_PASS'] ?? 'wordpress'));
+putenv("WORDPRESS_DB_HOST=127.0.0.1:" . ($_ENV['FORWARD_DB_PORT'] ?? '3306'));
