973d5f2500b7a98a3cf573ee18ecc882db764bd6
Fery Wardiyanto <ferywardiyanto@gmail.com>
2026-06-03 18:16:50 +0700

    chore: just update
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/integrations/TabellioCF7/PluginTest.php
+++ tests/integrations/TabellioCF7/PluginTest.php
@@ -24,10 +24,8 @@ class PluginTest extends TestCase
         Plugin::wpcf7_init();
 
         // Assert: Verify that essential filters and actions are registered.
-        // We use has_filter as has_action is an alias for it.
 
         // Check if admin menu registration is registered.
-        // Since add_admin_page calls add_action('admin_menu', ...), we verify that.
         $this->assertNotFalse(
             \has_action('admin_menu'),
             'Admin menu registration should be hooked to admin_menu.'
